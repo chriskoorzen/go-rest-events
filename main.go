859ab8203d37dacbddcc22285293c93d308ce8b7
Chris Koorzen <fckoorzen@gmail.com>
2025-01-12 13:04:24 +0200

    Add create and retrieve logic for events

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/chriskoorzen/go-rest-demo/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,13 +27,46 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+
 	context.JSON(http.StatusOK, gin.H{
-		"message": "GET all events",
+		"events": events,
 	})
 }
 
 func createEvent(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "POSTed an event",
+	devOutputBodyToConsole(context) // output the raw body to console
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse POST request",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	// If binding is successful, try to save the event
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not save event",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	// return newly created event
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "POST successful",
+		"event":   event,
 	})
+}
+
+func devOutputBodyToConsole(context *gin.Context) {
+	// output the raw body for dev purposes
+	body, _ := io.ReadAll(context.Request.Body)
+	context.Request.Body = io.NopCloser(bytes.NewBuffer(body)) // Reset the request body
+	fmt.Println("Raw Body:", string(body))
 }
