c434338f443185c4831618eb8604866456ef2168
Chris Koorzen <fckoorzen@gmail.com>
2025-01-12 11:52:42 +0200

    Setup Gin framework

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello, REST API!")
+	server := gin.Default()
+
+	server.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "hello REST API",
+		})
+	})
+
+	server.Run(":8080")
 }
