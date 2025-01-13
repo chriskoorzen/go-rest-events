04b25ef9eb99758add9b91bec72fb3040c512be0
Chris Koorzen <fckoorzen@gmail.com>
2025-01-13 09:21:15 +0200

    Fix route wildcard names

--- routes/routes.go
+++ routes/routes.go
@@ -21,8 +21,8 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events/:eventsID", getSingleEvent)
 	protected.PUT("/events/:eventsID", updateEvent)
 	protected.DELETE("/events/:eventsID", deleteEvent)
-	protected.POST("/events/:id/register", registerForEvent)
-	protected.DELETE("/events/:id/register", cancelRegistration)
+	protected.POST("/events/:eventsID/register", registerForEvent)
+	protected.DELETE("/events/:eventsID/register", cancelRegistration)
 
 	server.POST("/users", createUser)
 	server.POST("/users/login", loginUser)
